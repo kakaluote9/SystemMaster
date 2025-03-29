6cf4bdc38b5bad79f7af4b39b208d9e6141195c9
kakaluote9 <49047971+kakaluote9@users.noreply.github.com>
2025-03-29 16:28:20 +0800

    Update main.py
    1

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ import routes  # noqa: F401
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
-
-
-    @app.route("/")  # 关键！必须定义根路径
-    def home():
-        return render_template("login.html")  # 或返回你的首页模板
