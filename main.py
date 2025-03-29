824138009d276b9a65c9402b02de5e7cc51b6900
kakaluote9 <49047971+kakaluote9@users.noreply.github.com>
2025-03-29 16:03:30 +0800

    Update main.py
    fix 404

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from flask import render_template
 from app import app
 import routes  # noqa: F401
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
+
+    @app.route("/")  # 关键！必须定义根路径
+    def home():
+        return render_template("login.html")  # 或返回你的首页模板
